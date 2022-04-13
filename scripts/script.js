3a6eaacab16d585b7d8d7ca17217932a411b21d8
Badi-Haki Martin <jojackblack@gmail.com>
2022-04-12 20:11:59 -0400

    added the hidden class. clicking on cards reveals the player search bar, now with a server list

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded',init);
 
 function init(event){
     console.log(`hello`);
+
+    const cards = document.querySelectorAll('.card');
+    for (const card of cards    ) {
+        card.addEventListener('click',selectCard);
+    }
+}
+
+function selectCard(event){
+    console.log(event.target);
+    const searchForm = document.querySelector(`#searchForm`);
+    searchForm.classList.remove('hidden')
 }
