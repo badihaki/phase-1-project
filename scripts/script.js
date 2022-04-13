e19194e081a0d3a3e0b56c9e578857fd6b95b9be
Badi-Haki Martin <jojackblack@gmail.com>
2022-04-12 20:44:13 -0400

    app now stores the values we are trying to search for. The server is a global-scoped variable that changes with the "change" event listener, and the player name is caught when you press the submit button

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,41 @@
+let server = `default`;
+
 document.addEventListener('DOMContentLoaded',init);
 
 function init(event){
-    console.log(`hello`);
-
-    const cards = document.querySelectorAll('.card');
-    for (const card of cards    ) {
-        card.addEventListener('click',selectCard);
+    // const cards = document.querySelectorAll('.card');
+    for (const card of document.querySelectorAll('.card')) {
+        card.addEventListener('click',clickOnCard);
     }
+
+    document.querySelector(`#serverSelect`).addEventListener('change',(event)=>{
+        server = event.target.value.toLowerCase();
+    })
 }
 
-function selectCard(event){
+function clickOnCard(event){
     console.log(event.target);
+    const card = event.target;
     const searchForm = document.querySelector(`#searchForm`);
     searchForm.classList.remove('hidden')
+
+    // add the submit event listener to the search button
+    document.querySelector(`#searchButton`).addEventListener('click',(searchEvent)=>{
+        searchEvent.preventDefault();
+        const nameToSearch = document.querySelector(`#searchName`).value.replaceAll(` `,`&`);
+        if(server != `default`){
+            console.log(`name to search: ${nameToSearch} // server to search in: ${server}`);
+            // remove the server warning if it exists
+            if(document.querySelector(`#noServerWarning`) != null){
+                document.querySelector(`#noServerWarning`).remove();
+            }
+            // build out the functionality with the API
+        }
+        else{
+            const serverWarning = document.createElement(`p`);
+            serverWarning.innerText = `Please select a server from the dropdown box`
+            serverWarning.id = `noServerWarning`
+            document.querySelector('#characterPanel').appendChild(serverWarning);
+        }
+    })
 }
