0ab63f248b955c9ef7bd717f2bcae13b783646f3
Badi-Haki Martin <jojackblack@gmail.com>
2022-04-17 15:37:32 -0400

    scaled back project. Took out party functionality

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,85 @@ let server = `default`;
 document.addEventListener('DOMContentLoaded',init);
 
 function init(event){
-    // const cards = document.querySelectorAll('.card');
-    for (const card of document.querySelectorAll('.card')) {
-        card.addEventListener('click',clickOnCard);
-    }
-
     document.querySelector(`#serverSelect`).addEventListener('change',(event)=>{
         server = event.target.value.toLowerCase();
     })
+
+    document.querySelector("#searchButton").addEventListener('click', searchForPlayer)
+}
+
+function searchForPlayer(searchEvent){
+    searchEvent.preventDefault();
+    const nameToSearch = document.querySelector(`#searchName`).value.replaceAll(` `,`&`);
+        if(server != `default` && nameToSearch != ''){
+            // clear old list data
+            clearPlayerList();
+            console.log(`name to search: ${nameToSearch} // server to search in: ${server}`);
+            // remove the server warning if it exists
+            if(document.querySelector(`#noServerWarning`) != null){
+                document.querySelector(`#noServerWarning`).remove();
+            }
+            // build out the functionality with the API
+            fetch(`https://xivapi.com/character/search?name=${nameToSearch}&server=${server}`)
+                .then(resp=>{
+                    document.querySelector(`#searchButton`).disabled = true;
+                    return resp.json()
+                })
+                    .then(data=>{
+                        // console.log(data['Results']);
+                        const playerList = data['Results'].slice(4);
+                        // debugger;
+                        console.log(playerList);
+                        for (const player of playerList) {
+                            const {Avatar, Name} = player;
+                            //debugger;
+                            const li = document.createElement(`li`);
+                            li.className = 'playerLI';
+                            li.id = player[`ID`];
+
+                            li.innerHTML = `
+                            <img src=${Avatar}>
+                            <h3>${Name}</h3>
+                            `
+                            li.addEventListener('click',(listEvent)=>{
+                                // I need to show the player information in #resultsDetails div
+                                fetch(`https://xivapi.com/character/${listEvent.target.parentNode.id}`).then(callForResp=>callForResp.json()).then(playerData=>{
+                                    console.log(playerData);
+                                    const {Name, ActiveClassJob,Portrait} = playerData[`Character`];
+                                    const playerName = document.createElement('h2');
+                                    playerName.innerText = Name;
+                                    const playerPortrait = document.createElement('img');
+                                    playerPortrait.src = Portrait;
+                                    const playerJob = document.createElement('h3');
+                                    playerJob.innerText = ActiveClassJob[`UnlockedState`][`Name`];
+                                    const playerJobLevel = document.createElement(`h4`);
+                                    playerJobLevel.innerText =  ActiveClassJob[`Level`];
+
+                                   const searchResults = document.createElement(`div`);
+                                   searchResults.id = `playerInfo`
+                                   clearPlayerInfo();
+                                   document.querySelector('#resultsDetails').appendChild(searchResults);
+
+                                   searchResults.appendChild(playerName);
+                                   searchResults.appendChild(playerPortrait);
+                                   searchResults.appendChild(playerJob);
+                                   searchResults.appendChild(playerJobLevel);
+
+                                })
+                            })
+                            document.querySelector('#searchResults').appendChild(li);
+                        }
+                        document.querySelector(`#searchButton`).disabled = false;
+                    })
+        }
+        else{
+            const serverWarning = document.createElement(`p`);
+            serverWarning.innerText = `Please select a server from the dropdown box and type a name into the input field to search `
+            serverWarning.id = `noServerWarning`
+            if(document.querySelector(`#noServerWarning`) === null){
+                document.querySelector('#characterPanel').appendChild(serverWarning);
+            }
+        }
 }
 
 function clickOnCard(event){
@@ -111,13 +182,13 @@ function clickOnCard(event){
                                        playerCardDiv.appendChild(playerJob);
                                        playerCardDiv.appendChild(playerJobLevel);
 
-                                       event.target.appendChild(playerCardDiv);
+                                       card.appendChild(playerCardDiv);
 
                                        document.querySelector(`#searchName`).value = '';
                                        searchForm.className = `hidden`;
                                        
-                                       event.target.querySelector('button').classList.remove('hidden');
-                                       event.target.querySelector('button').addEventListener('click',removePartyMember);
+                                       card.querySelector('button').classList.remove('hidden');
+                                       card.querySelector('button').addEventListener('click',removePartyMember);
 
                                        event.target.removeEventListener('click',clickOnCard);
                                    })
